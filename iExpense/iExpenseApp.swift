d24f8e22249528997862288826a3f33746fb8c6f
Rob Ranf <rob@emiyaconsulting.com>
2023-04-17 15:35:28 -0700

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Rob Ranf on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
