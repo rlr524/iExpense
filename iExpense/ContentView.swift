2e2046870fe8cb07a4f84553181f708dfdeaa34e
Rob Ranf <rob@emiyaconsulting.com>
2023-04-17 19:01:20 -0700

    Starting code for wrappers

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,15 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    // Note: when creating the shared data use @StateObject, but when you’re just using it in a
+    // different view you should use @ObservedObject instead.
+    @StateObject var user = User()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Your name is \(user.firstName) \(user.lastName)")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
         }
         .padding()
     }
+}
+
+// Because @State only works on structs for internal struct state, we need to use the
+// @Published property observer in addition to the @StateObject property wrapper to
+// pass state around views vs just inside a struct.
+
+// @Published functions in a way like half of how @State functions, it tells Swift that
+// whenever the property changes, it should broadcast that change to any SwiftUI views
+// that are watching so they should reload. The other half, the part that says which
+// instantiated object should be watched, is wrapped using @StateObject.
+
+// Passing ObservableObject as an inherited type alias makes the class conform to the
+// ObservableObject protocol, which is required to use @StateObject when we
+// instantiate the class above.
+class User: ObservableObject {
+    @Published var firstName = "Madison"
+    @Published var lastName = "Ranf"
 }
 
 struct ContentView_Previews: PreviewProvider {
